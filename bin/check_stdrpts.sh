601907bf25200cc976fd9cf413fbd4de3e1fabc0
andrew-pickin-epi <Andrew.Pickin@epimorphics.com>
2022-11-29 12:26:58 +0000

    Timing can be given on cmd line

--- bin/check_stdrpts.sh
+++ bin/check_stdrpts.sh
@@ -7,21 +7,21 @@ UNKNOWN=3
 
 PROG=$(basename $0)
 
-SCAN=60m
-GRACE=1800
+SCAN=${1:-60}m
+GRACE=$((60*${2:-30}))
 
 check () 
 {
   REPORTS=$(docker logs --since=$SCAN std-rpts-mgr 2>&1 \
   | jq -r '. | select(has("report-id")) | {ts: .ts, state: .state, name: ."report-id"}' \
-  | jq -rs '.? | map( { (.name): . } ) | add | .[] | select(.state!="Completed") | . + {"elapsed":( now - (.ts | sub("\\.[0-9]+Z$"; "Z")| fromdate ) | floor)} | select(.elapsed>'$GRACE') | [.name,.elapsed] | @csv' )
+  | jq -rs '.? | map( { (.name): . } ) | add | .[]? | select(.state!="Completed") | . + {"elapsed":( now - (.ts | sub("\\.[0-9]+Z$"; "Z")| fromdate ) | floor)} | select(.elapsed>'$GRACE') | [.name,.elapsed] | @csv' )
 
   # first jq filters all the noise - only interested in actual reports.
   #   (also converts report-id to name so I don't have to keep quoting report-id)
   # The second (actually it should be possible to continue the first line but you can't for reasons):
   # -s put everything in one big array.
   # .? don't carp if you got nothing.
-  # map( { (.name): . } ) | add | .[] covert into a dictionary keyed on name .
+  # map( { (.name): . } ) | add | .[]? covert into a dictionary keyed on name .
   #   This has the effect of only giving the last entry per report.
   # select(.state!="Completed") | . + {"elapsed":( now - (.ts | sub("\\.[0-9]+Z$"; "Z")| fromdate ) | floor)}'
   #   ignore Comlpeted,
